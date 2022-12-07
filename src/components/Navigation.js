f2dd2bfc72c386a0395c17398f7a743b76ce7b2d
Shrikrushna Patil <patilshrikrushna07@gmail.com>
2022-12-07 22:04:55 +0530

    main

---
+++ src/components/Navigation.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { Nav, Navbar, Container, Button, NavDropdown } from "react-bootstrap";
+import { useLogoutUserMutation } from "../services/appApi";
+import { useSelector } from "react-redux";
+import { LinkContainer } from "react-router-bootstrap";
+// import logo from "../assets/logo.png";
+function Navigation() {
+    const user = useSelector((state) => state.user);
+    const [logoutUser] = useLogoutUserMutation();
+    async function handleLogout(e) {
+        e.preventDefault();
+        await logoutUser(user);
+        // redirect to home page
+        window.location.replace("/");
+    }
+    return (
+        <Navbar bg="light" expand="lg">
+            <Container>
+              
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ms-auto">
+                        {!user && (
+                            <LinkContainer to="/login">
+                                <Nav.Link>Login</Nav.Link>
+                            </LinkContainer>
+                        )}
+                        {user && (
+                            <NavDropdown
+                                title={
+                                    <>
+                                        <img src={user.picture} style={{ width: 30, height: 30, marginRight: 10, objectFit: "cover", borderRadius: "50%" }} />
+                                        {user.name}
+                                    </>
+                                }
+                                id="basic-nav-dropdown"
+                            >
+                             
+                                <NavDropdown.Item>
+                                    <Button variant="danger" onClick={handleLogout}>
+                                        Logout
+                                    </Button>
+                                </NavDropdown.Item>
+                            </NavDropdown>
+                        )}
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
+
+export default Navigation;
