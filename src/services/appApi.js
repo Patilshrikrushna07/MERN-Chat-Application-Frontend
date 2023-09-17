33cf502b7b10431705b4107fb52e0d8a3df6e44e
Shrikrushna Patil <shri.medisage@gmail.com>
2023-09-17 13:48:42 +0530

    add changes

--- src/services/appApi.js
+++ src/services/appApi.js
@@ -5,7 +5,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const appApi = createApi({
     reducerPath: "appApi",
     baseQuery: fetchBaseQuery({
-        baseUrl: "http://localhost:5001",
+        baseUrl: "https://chat-app-backend-q6tv.onrender.com",
     }),
 
     endpoints: (builder) => ({
